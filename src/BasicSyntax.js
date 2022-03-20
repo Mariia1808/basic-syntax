0d3dc940fa65b7e636b275b13c5b2be74e07ff05
Maria1808 <mvbannikova1808@gmail.com>
2022-03-20 21:31:26 +0500

    1

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,33 @@
 export function romanToInteger(str) {
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    for (let i = 0; i < str.length; i++) {
+        let current = char_to_int(str.charAt(i));
+        let next = char_to_int(str.charAt(i + 1));
+        if (current >= next) {
+            result += current;
+        } else {
+            result -= current;
+        }
+    }
     return result;
+}
+function char_to_int(character) {
+    switch (character) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return -1;
+    }
 }
